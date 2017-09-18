f80e53ef1e6d8efd1f29aa134a5e417e4fa64586
Paul Glendenning <paul@marbles.ai>
2017-09-18 15:53:03 -0700

    Initial revision

---
+++ src/test/java/ai/marbles/grpc/ServiceTest.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) Marbles AI Corp. 2016-2017.
+ * All rights reserved.
+ * Author: Paul Glendenning
+ */
+
+package ai.marbles.grpc;
+
+//Java packages
+import com.google.protobuf.Empty;
+import io.grpc.stub.StreamObserver;
+import com.google.common.util.concurrent.ListenableFuture;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class ServiceTest {
+	public class TestHandler extends DiscoveryGrpc.DiscoveryImplBase  {
+		@Override
+		public void ping(Empty request, StreamObserver<Empty> responseObserver) {
+			responseObserver.onNext(Empty.newBuilder().build());
+			responseObserver.onCompleted();
+		}
+
+		@Override
+		public void endpoints(Empty request, StreamObserver<ServiceEndpoints> responseObserver) {
+			ServiceEndpoints ep = ServiceEndpoints.newBuilder()
+					.addEndpoints(Endpoint.newBuilder().setName("test123").build())
+					.build();
+			responseObserver.onNext(ep);
+			responseObserver.onCompleted();
+		}
+
+		@Override
+		public void configure(Configuration request, StreamObserver<ConfigResult> responseObserver) {
+			responseObserver.onNext(ConfigResult.newBuilder()
+					.setStatus(ConfigResult.Status.WARNING).build());
+			responseObserver.onCompleted();
+		}
+	}
+
+	@Test
+	public void testSyncClientSyncServer() {
+
+		try {
+			ServiceAcceptor server = new ServiceAcceptor(9001, new TestHandler());
+			server.start();
+
+			ServiceConnector client = new ServiceConnector("localhost", 9001);
+			DiscoveryGrpc.DiscoveryBlockingStub stub = DiscoveryGrpc.newBlockingStub(client.getChannel());
+
+			ServiceEndpoints ep = stub.endpoints(Empty.newBuilder().build());
+			assertTrue(ep != null);
+			assertTrue(ep.getEndpointsCount() == 1);
+			assertTrue(ep.getEndpoints(0).getName().equals("test123"));
+
+			ConfigResult conf = stub.configure(Configuration.newBuilder().build());
+			assertTrue(conf != null);
+			assertTrue(conf.getStatus().equals(ConfigResult.Status.WARNING));
+
+			assertTrue(client.shutdown().blockUntilShutdown(3000));
+			assertTrue(server.shutdown().blockUntilShutdown(3000));
+		} catch(Exception e) {
+			fail(e.getMessage());
+		}
+	}
+
+	@Test
+	public void testAsyncClientSyncServer() {
+		try {
+			ServiceAcceptor server = new ServiceAcceptor(9002, new TestHandler());
+			server.start();
+
+			ServiceConnector client = new ServiceConnector("localhost", 9002);
+			DiscoveryGrpc.DiscoveryFutureStub stub = DiscoveryGrpc.newFutureStub(client.getChannel());
+
+			ListenableFuture<ServiceEndpoints> rpc1 = stub.endpoints(Empty.newBuilder().build());
+			ServiceEndpoints ep = rpc1.get();
+			assertTrue(ep != null);
+			assertTrue(ep.getEndpointsCount() == 1);
+			assertTrue(ep.getEndpoints(0).getName().equals("test123"));
+
+			ListenableFuture<ConfigResult> rpc2 = stub.configure(Configuration.newBuilder().build());
+			ConfigResult conf = rpc2.get();
+			assertTrue(conf != null);
+			assertTrue(conf.getStatus().equals(ConfigResult.Status.WARNING));
+
+			assertTrue(client.shutdown().blockUntilShutdown(3000));
+			assertTrue(server.shutdown().blockUntilShutdown(3000));
+		} catch(Exception e) {
+			fail(e.getMessage());
+		}
+	}
+}
