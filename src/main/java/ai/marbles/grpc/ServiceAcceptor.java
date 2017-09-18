0c17f4ba01391d57f980815e5d3484c3e187e175
Paul Glendenning <paul@marbles.ai>
2017-09-18 16:14:58 -0700

    Convert indents to spaces.

--- src/main/java/ai/marbles/grpc/ServiceAcceptor.java
+++ src/main/java/ai/marbles/grpc/ServiceAcceptor.java
@@ -20,100 +20,100 @@ import org.apache.log4j.Logger;
  * A gRPC server for the marbles services.
  */
 public class ServiceAcceptor {
-	private static final Logger logger = LogManager.getLogger(ServiceAcceptor.class);
+    private static final Logger logger = LogManager.getLogger(ServiceAcceptor.class);
 
-	private final int port_;
-	private final Server server_;
+    private final int port_;
+    private final Server server_;
 
-	/**
-	  * Create a server listening on {@code port} using service to handle requests.
-	  *
-	  * @param  port    The port to listen on
-	  * @param  service The service used to handle requests.
-	  */
-	public ServiceAcceptor(int port, io.grpc.BindableService service) throws IOException {
-		this(ServerBuilder.forPort(port), port, service);
-	}
+    /**
+      * Create a server listening on {@code port} using service to handle requests.
+      *
+      * @param  port    The port to listen on
+      * @param  service The service used to handle requests.
+      */
+    public ServiceAcceptor(int port, io.grpc.BindableService service) throws IOException {
+        this(ServerBuilder.forPort(port), port, service);
+    }
 
-	/**
-	 * Create a server using serverBuilder as a base and using service to handle requests.
-	 */
-	public ServiceAcceptor(ServerBuilder<?> serverBuilder, int port, io.grpc.BindableService service) {
-		this.port_ = port;
-		server_ = serverBuilder.addService(service)
-				.build();
-	}
+    /**
+     * Create a server using serverBuilder as a base and using service to handle requests.
+     */
+    public ServiceAcceptor(ServerBuilder<?> serverBuilder, int port, io.grpc.BindableService service) {
+        this.port_ = port;
+        server_ = serverBuilder.addService(service)
+                .build();
+    }
 
-	/**
-	 * Server accessor.
-	 *
-	 * @return The server.
-	 */
-	public Server getServer() {
-		return server_;
-	}
+    /**
+     * Server accessor.
+     *
+     * @return The server.
+     */
+    public Server getServer() {
+        return server_;
+    }
 
-	/**
-	 * Start serving requests. Requests are handled in daemon threads.
-	 */
-	public void start() throws IOException {
-		server_.start();
-		logger.info("Server started, listening on " + port_);
-		Runtime.getRuntime().addShutdownHook(new Thread() {
-			@Override
-			public void run() {
-				// Use stderr here since the logger may have been reset by its JVM shutdown hook.
-				System.err.println("*** shutting down gRPC server since JVM is shutting down");
-				ServiceAcceptor.this.shutdown();
-				System.err.println("*** server shut down");
-			}
-		});
-	}
+    /**
+     * Start serving requests. Requests are handled in daemon threads.
+     */
+    public void start() throws IOException {
+        server_.start();
+        logger.info("Server started, listening on " + port_);
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+                System.err.println("*** shutting down gRPC server since JVM is shutting down");
+                ServiceAcceptor.this.shutdown();
+                System.err.println("*** server shut down");
+            }
+        });
+    }
 
-	/**
-	 * Stop serving requests and shutdown resources.
-	 */
-	public ServiceAcceptor shutdown() {
-		if (server_ != null) {
-			server_.shutdown();
-		}
-		return this;
-	}
+    /**
+     * Stop serving requests and shutdown resources.
+     */
+    public ServiceAcceptor shutdown() {
+        if (server_ != null) {
+            server_.shutdown();
+        }
+        return this;
+    }
 
-	/**
-	 * Stop serving requests and shutdown resources.
-	 *
-	 * @param   force   Force an immediate shutdown.
-	 */
-	public ServiceAcceptor shutdown(boolean force) {
-		if (server_ != null) {
-			if (force)
-				server_.shutdownNow();
-			else
-				server_.shutdown();
-		}
-		return this;
-	}
+    /**
+     * Stop serving requests and shutdown resources.
+     *
+     * @param   force   Force an immediate shutdown.
+     */
+    public ServiceAcceptor shutdown(boolean force) {
+        if (server_ != null) {
+            if (force)
+                server_.shutdownNow();
+            else
+                server_.shutdown();
+        }
+        return this;
+    }
 
-	/**
-	 * Await termination on the main thread since the grpc library uses daemon threads.
-	 */
-	public void blockUntilShutdown() throws InterruptedException {
-		if (server_ != null) {
-			server_.awaitTermination();
-		}
-	}
+    /**
+     * Await termination on the main thread since the grpc library uses daemon threads.
+     */
+    public void blockUntilShutdown() throws InterruptedException {
+        if (server_ != null) {
+            server_.awaitTermination();
+        }
+    }
 
-	/**
-	 * Await termination on the main thread since the grpc library uses daemon threads.
-	 *
-	 * @param timeout   Timeout in milliseconds.
-	 * @return          True if shutdown completed. False on timeout.
-	 */
-	public boolean blockUntilShutdown(long timeout) throws InterruptedException {
-		if (server_ != null) {
-			return server_.awaitTermination(timeout, MILLISECONDS);
-		}
-		return true;
-	}
+    /**
+     * Await termination on the main thread since the grpc library uses daemon threads.
+     *
+     * @param timeout   Timeout in milliseconds.
+     * @return          True if shutdown completed. False on timeout.
+     */
+    public boolean blockUntilShutdown(long timeout) throws InterruptedException {
+        if (server_ != null) {
+            return server_.awaitTermination(timeout, MILLISECONDS);
+        }
+        return true;
+    }
 }
--- src/main/java/ai/marbles/grpc/ServiceConnector.java
+++ src/main/java/ai/marbles/grpc/ServiceConnector.java
@@ -33,102 +33,102 @@ import ai.marbles.grpc.DiscoveryGrpc;
  * Lucida client of gRPC service.
  */
 public final class ServiceConnector {
-	private static final Logger logger = LogManager.getLogger(ServiceConnector.class);
+    private static final Logger logger = LogManager.getLogger(ServiceConnector.class);
 
-	private final ManagedChannel channel_;
-	private final SecureRandom random_ = new SecureRandom();
+    private final ManagedChannel channel_;
+    private final SecureRandom random_ = new SecureRandom();
 
-	/**
-	 * Construct client for accessing a Lucida service at {@code host:port}.
-	 * If port=443 then the channel will be secure via TLS, otherwise the channel
-	 * is insecure. No authentication is provided in either case.
-	 *
-	 * @param host  Fully qualified host name
-	 * @param port  The port number [1,65536)
-	 */
-	public ServiceConnector(String host, int port) {
-		this(ManagedChannelBuilder.forAddress(host, port).usePlaintext(true));
-	}
+    /**
+     * Construct client for accessing a Lucida service at {@code host:port}.
+     * If port=443 then the channel will be secure via TLS, otherwise the channel
+     * is insecure. No authentication is provided in either case.
+     *
+     * @param host  Fully qualified host name
+     * @param port  The port number [1,65536)
+     */
+    public ServiceConnector(String host, int port) {
+        this(ManagedChannelBuilder.forAddress(host, port).usePlaintext(true));
+    }
 
-	/**
-	 * Construct client for accessing a Lucida service using an existing channel.
-	 * The channel credentials and encryption is dictated by the channelBuilder.
-	 *
-	 * @param channelBuilder The channel builder.
-	 * @see io.grpc.ManagedChannelBuilder
-	 */
-	public ServiceConnector(ManagedChannelBuilder<?> channelBuilder) {
-		channel_ = channelBuilder.build();
-	}
+    /**
+     * Construct client for accessing a Lucida service using an existing channel.
+     * The channel credentials and encryption is dictated by the channelBuilder.
+     *
+     * @param channelBuilder The channel builder.
+     * @see io.grpc.ManagedChannelBuilder
+     */
+    public ServiceConnector(ManagedChannelBuilder<?> channelBuilder) {
+        channel_ = channelBuilder.build();
+    }
 
-	/**
-	 * Channel accessor.
-	 *
-	 * @return The io.grpc.Channel for this client.
-	 */
-	public Channel getChannel() {
-		return channel_;
-	}
+    /**
+     * Channel accessor.
+     *
+     * @return The io.grpc.Channel for this client.
+     */
+    public Channel getChannel() {
+        return channel_;
+    }
 
-	/**
-	 * Shutdown the client connection gracefully.
-	 */
-	public ServiceConnector shutdown() {
-		if (channel_ != null) {
-			channel_.shutdown();
-		}
-		return this;
-	}
+    /**
+     * Shutdown the client connection gracefully.
+     */
+    public ServiceConnector shutdown() {
+        if (channel_ != null) {
+            channel_.shutdown();
+        }
+        return this;
+    }
 
-	/**
-	 * Shutdown the client connection.
-	 *
-	 * @param   force   Force an immediate shutdown.
-	 */
-	public ServiceConnector shutdown(boolean force) {
-		if (channel_ != null) {
-			if (force)
-				channel_.shutdownNow();
-			else
-				channel_.shutdown();
-		}
-		return this;
-	}
+    /**
+     * Shutdown the client connection.
+     *
+     * @param   force   Force an immediate shutdown.
+     */
+    public ServiceConnector shutdown(boolean force) {
+        if (channel_ != null) {
+            if (force)
+                channel_.shutdownNow();
+            else
+                channel_.shutdown();
+        }
+        return this;
+    }
 
-	/**
-	 * Await termination on the main thread since the grpc library uses daemon threads.
-	 */
-	public void blockUntilShutdown() throws InterruptedException {
-		if (channel_ != null) {
-			while (channel_.awaitTermination(365, DAYS)) {
-				/* do nothing */
-			}
-		}
-	}
+    /**
+     * Await termination on the main thread since the grpc library uses daemon threads.
+     */
+    public void blockUntilShutdown() throws InterruptedException {
+        if (channel_ != null) {
+            while (channel_.awaitTermination(365, DAYS)) {
+                /* do nothing */
+            }
+        }
+    }
 
-	/**
-	 * Await termination on the main thread since the grpc library uses daemon threads.
-	 *
-	 * @param timeout   Timeout in milliseconds.
-	 * @return          True if shutdown completed. False on timeout.
-	 */
-	public boolean blockUntilShutdown(long timeout) throws InterruptedException {
-		if (channel_ != null) {
-			return channel_.awaitTermination(timeout, MILLISECONDS);
-		}
-		return true;
-	}
+    /**
+     * Await termination on the main thread since the grpc library uses daemon threads.
+     *
+     * @param timeout   Timeout in milliseconds.
+     * @return          True if shutdown completed. False on timeout.
+     */
+    public boolean blockUntilShutdown(long timeout) throws InterruptedException {
+        if (channel_ != null) {
+            return channel_.awaitTermination(timeout, MILLISECONDS);
+        }
+        return true;
+    }
 
-	/**
-	 * Get supported services
-	 */
-	public ServiceEndpoints discoverEndpoints() throws UnknownServiceException {
-		DiscoveryGrpc.DiscoveryBlockingStub stub = DiscoveryGrpc.newBlockingStub(channel_);
-		try {
-			return stub.endpoints(Empty.newBuilder().build());
-		} catch (StatusRuntimeException e) {
-			logger.warn("RPC failed", e);
-			throw new UnknownServiceException(e.getMessage());
-		}
-	}
+    /**
+     * Get supported services
+     */
+    public ServiceEndpoints discoverEndpoints() throws UnknownServiceException {
+        DiscoveryGrpc.DiscoveryBlockingStub stub = DiscoveryGrpc.newBlockingStub(channel_);
+        try {
+            return stub.endpoints(Empty.newBuilder().build());
+        } catch (StatusRuntimeException e) {
+            logger.warn("RPC failed", e);
+            throw new UnknownServiceException(e.getMessage());
+        }
+    }
 }
